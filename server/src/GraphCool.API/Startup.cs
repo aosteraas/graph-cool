1025fc6391fff1dc35d51e0dd1e2cd122cf7d736
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-18 06:09:07 +1100

    Switch to GraphQL included playground, allow sync io for playground, incject serivice lcoator, add gql to app

--- server/src/GraphCool.API/Startup.cs
+++ server/src/GraphCool.API/Startup.cs
@@ -1,13 +1,18 @@
-using GraphiQl;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
 using GraphQL.Http;
 using GraphCool.API.Data;
 using Microsoft.EntityFrameworkCore;
+using GraphCool.API.Services;
+using GraphCool.API.Interfaces;
+using GraphCool.API.Query;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace GraphCool.API
 {
@@ -23,13 +28,23 @@ namespace GraphCool.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(opts =>
+            {
+                opts.AllowSynchronousIO = true;
+            });
             services.AddDbContext<CoolContext>(opts => opts.UseSqlite("Data Source=./Data/graphcool.db"));
 
             services.AddControllers();
             services.AddHttpContextAccessor();
+            services.AddSingleton<ContextServiceLocator>();
 
             services.AddSingleton<GraphQL.IDocumentExecuter, GraphQL.DocumentExecuter>();
             services.AddSingleton<GraphQL.Http.IDocumentWriter, GraphQL.Http.DocumentWriter>();
+            services.AddGraphQL(_ =>
+            {
+                _.EnableMetrics = true;
+                _.ExposeExceptions = true;
+            }).AddGraphTypes(ServiceLifetime.Scoped);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +57,10 @@ namespace GraphCool.API
 
             app.UseHttpsRedirection();
 
+            app.UseGraphQL<ISchema>();
+
+            app.UseGraphQLPlayground();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -51,7 +70,6 @@ namespace GraphCool.API
                 endpoints.MapControllers();
             });
 
-            app.UseGraphiQl();
         }
     }
 }
