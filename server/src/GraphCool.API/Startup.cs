22ccef5c451f40a6111968bc3cf55e867c432e7f
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-17 17:09:25 +1100

    add dotnet api project and sln

---
+++ server/src/GraphCool.API/Startup.cs
@@ -0,0 +1,53 @@
+using GraphiQl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GraphQL;
+using GraphQL.Http;
+
+namespace GraphCool.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton<GraphQL.IDocumentExecuter, GraphQL.DocumentExecuter>();
+            services.AddSingleton<GraphQL.Http.IDocumentWriter, GraphQL.Http.DocumentWriter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseGraphiQl();
+        }
+    }
+}
