e060bcae0dc26c7ab454b02997c202ee59ae7b9e
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-19 06:57:05 +1100

    init ios app

---
+++ iOS/BagelGram/BagelGram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BagelGram
+//
+//  Created by Aaron Osteraas on 19/11/19.
+//  Copyright © 2019 Aaron Osteraas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
