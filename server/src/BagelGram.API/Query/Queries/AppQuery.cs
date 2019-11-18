4173a08f548729cdadceb097d520dc7e5188ff5e
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-18 21:15:16 +1100

    fix up query args

--- server/src/BagelGram.API/Query/Queries/AppQuery.cs
+++ server/src/BagelGram.API/Query/Queries/AppQuery.cs
@@ -34,10 +34,11 @@ namespace BagelGram.API.Query.Queries
 
             FieldAsync<ImageType>(
                 name: "Image",
-                description: "All images for user",
+                description: "A single image",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "imageId", Description = "The Image's Id" }),
                 resolve: async context =>
                 {
-                    var id = context.GetArgument<int>("id");
+                    var id = context.GetArgument<int>("imageId");
                     var image = await locator.ImageRepository.GetImageAsync(id);
                     return image;
                 }
@@ -46,12 +47,27 @@ namespace BagelGram.API.Query.Queries
             FieldAsync<ListGraphType<ImageType>>(
                 name: "imagesForUser",
                 description: "All Images for a User",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "userId", Description = "The User's Id" }),
                 resolve: async context =>
                 {
                     var userId = context.GetArgument<int>("userId");
                     var images = await locator.ImageRepository.GetUserImages(userId);
                     return images;
                 }
+            );
+
+            FieldAsync<ListGraphType<CommentType>>(
+                name: "commentsForImage",
+                description: "All comments on a given image",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "imageId", Description = "The Image's Id" }),
+                resolve: async context => await locator.CommentRepository.GetImageComments(context.GetArgument<int>("imageId"))
+            );
+
+            FieldAsync<ListGraphType<CommentType>>(
+                name: "commentsForUser",
+                description: "All comments from a given user",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "userId", Description = "The User's Id" }),
+                resolve: async context => await locator.CommentRepository.GetImageComments(context.GetArgument<int>("userId"))
             );
         }
 
