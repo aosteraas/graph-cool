16a578507cb1a6af574492f726005da89f0fc6e2
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-19 17:52:08 +1100

    tidy up

--- server/src/BagelGram.Data/ModelBuilderExtensions.cs
+++ server/src/BagelGram.Data/ModelBuilderExtensions.cs
@@ -27,15 +27,15 @@ namespace BagelGram.Data
         {
             const string uploadDir = "/uploads/";
             var sources = new List<string>() {
-                $"{uploadDir}2019-11-19-at-5.17.46.png" ,
-                $"{uploadDir}2019-11-19-at-5.18.30.png" ,
-                $"{uploadDir}2019-11-19-at-5.19.01.png" ,
-                $"{uploadDir}2019-11-19-at-5.19.40.png" ,
-                $"{uploadDir}2019-11-19-at-5.20.02.png" ,
-                $"{uploadDir}2019-11-19-at-5.20.28.png" ,
-                $"{uploadDir}2019-11-19-at-5.20.45.png" ,
-                $"{uploadDir}2019-11-19-at-5.21.00.png" ,
-                $"{uploadDir}2019-11-19-at-5.21.26.png" ,
+                $"{uploadDir}2019-11-19-at-5.17.46.png",
+                $"{uploadDir}2019-11-19-at-5.18.30.png",
+                $"{uploadDir}2019-11-19-at-5.19.01.png",
+                $"{uploadDir}2019-11-19-at-5.19.40.png",
+                $"{uploadDir}2019-11-19-at-5.20.02.png",
+                $"{uploadDir}2019-11-19-at-5.20.28.png",
+                $"{uploadDir}2019-11-19-at-5.20.45.png",
+                $"{uploadDir}2019-11-19-at-5.21.00.png",
+                $"{uploadDir}2019-11-19-at-5.21.26.png",
                 $"{uploadDir}2019-11-19-at-5.21.52.png"
             };
 
