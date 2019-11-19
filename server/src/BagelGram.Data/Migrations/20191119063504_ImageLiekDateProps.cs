03ef407c0c42277732b8bf56b8450c1295456c7f
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-19 17:35:28 +1100

    Add migration

---
+++ server/src/BagelGram.Data/Migrations/20191119063504_ImageLiekDateProps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BagelGram.Data.Migrations
+{
+    public partial class ImageLiekDateProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Likes",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Uploaded",
+                table: "Images",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "Uploaded",
+                table: "Images");
+        }
+    }
+}
