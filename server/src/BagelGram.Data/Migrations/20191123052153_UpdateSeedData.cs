3a4ece9698acadb14013da197613b3386263c95e
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-11-23 16:22:46 +1100

    Update seed data

---
+++ server/src/BagelGram.Data/Migrations/20191123052153_UpdateSeedData.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BagelGram.Data.Migrations
+{
+    public partial class UpdateSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Caption",
+                table: "Images",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Probably almost Bagel's first day home", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Chew toy on the head", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Smushed head dog", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "First lazy sit captured", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "And onto her second collar", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Frowning head bagel", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "World's most forlorn dog", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Needy ass dog", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "She used to fit on a chair", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Caption", "Uploaded" },
+                values: new object[] { "Deer-ear dog", new DateTime(2019, 11, 13, 16, 21, 52, 967, DateTimeKind.Local).AddTicks(2810) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Caption",
+                table: "Images");
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Images",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Uploaded",
+                value: new DateTime(2019, 11, 9, 17, 49, 8, 573, DateTimeKind.Local).AddTicks(8610));
+        }
+    }
+}
--- server/src/BagelGram.Data/ModelBuilderExtensions.cs
+++ server/src/BagelGram.Data/ModelBuilderExtensions.cs
@@ -26,30 +26,31 @@ namespace BagelGram.Data
         private static Image[] GenerateImageData()
         {
             const string uploadDir = "/uploads/";
-            var sources = new List<string>() {
-                $"{uploadDir}2019-11-19-at-5.17.46.png",
-                $"{uploadDir}2019-11-19-at-5.18.30.png",
-                $"{uploadDir}2019-11-19-at-5.19.01.png",
-                $"{uploadDir}2019-11-19-at-5.19.40.png",
-                $"{uploadDir}2019-11-19-at-5.20.02.png",
-                $"{uploadDir}2019-11-19-at-5.20.28.png",
-                $"{uploadDir}2019-11-19-at-5.20.45.png",
-                $"{uploadDir}2019-11-19-at-5.21.00.png",
-                $"{uploadDir}2019-11-19-at-5.21.26.png",
-                $"{uploadDir}2019-11-19-at-5.21.52.png"
+            var sourceCaptionData = new List<(string source, string caption)>() {
+                ($"{uploadDir}2019-11-19-at-5.17.46.png", "Probably almost Bagel's first day home"),
+                ($"{uploadDir}2019-11-19-at-5.18.30.png", "Chew toy on the head"),
+                ($"{uploadDir}2019-11-19-at-5.19.01.png", "Smushed head dog"),
+                ($"{uploadDir}2019-11-19-at-5.19.40.png", "First lazy sit captured"),
+                ($"{uploadDir}2019-11-19-at-5.20.02.png", "And onto her second collar"),
+                ($"{uploadDir}2019-11-19-at-5.20.28.png", "Frowning head bagel"),
+                ($"{uploadDir}2019-11-19-at-5.20.45.png", "World's most forlorn dog"),
+                ($"{uploadDir}2019-11-19-at-5.21.00.png", "Needy ass dog"),
+                ($"{uploadDir}2019-11-19-at-5.21.26.png", "She used to fit on a chair"),
+                ($"{uploadDir}2019-11-19-at-5.21.52.png", "Deer-ear dog")
             };
 
-            var startDate = DateTime.Now.AddDays(sources.Count * -1);
+            var startDate = DateTime.Now.AddDays(sourceCaptionData.Count * -1);
 
             var imageData = new List<Image>();
             var id = 1;
-            sources.ForEach(source =>
+            sourceCaptionData.ForEach(scd =>
             {
                 imageData.Add(new Image
                 {
                     Id = id,
                     UserId = 1,
-                    Source = source,
+                    Source = scd.source,
+                    Caption = scd.caption,
                     Uploaded = startDate
                 });
                 id++;
